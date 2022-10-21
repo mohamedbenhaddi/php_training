b0139c7ebd456309a724bdfe1918b4bfc13fb640
mohamedbenhaddi <116380278+mohamedbenhaddi@users.noreply.github.com>
2022-10-21 21:17:38 +0200

    Update debutbddpdo.php

--- debutbddpdo.php
+++ debutbddpdo.php
@@ -16,3 +16,15 @@ foreach ($posts as $post) {
 
     echo $post['title'].PHP_EOL;
 }
+
+//prepare
+
+$id = 2;
+$pdo = new PDO('mysql:host=localhost;dbname=php','root','');
+
+$posts = $pdo->prepare('SELECT * FROM posts WHERE id = :id');
+$posts->bindParam(':id', $id, PDO::PARAM_INT);
+
+$posts->execute();
+
+var_dump($posts->fetch(PDO::FETCH_ASSOC));

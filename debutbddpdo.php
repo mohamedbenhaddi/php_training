627bb43a60b2d3881440e9f8df3f94962e1494c3
mohamedbenhaddi <116380278+mohamedbenhaddi@users.noreply.github.com>
2022-10-21 21:12:31 +0200

    Create debutbddpdo.php

---
+++ debutbddpdo.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types = 1);
+
+$pdo = new PDO('mysql:host=localhost;dbname=php','root','');
+
+$posts = $pdo->query('SELECT * FROM posts');
+
+//while($post = $posts->fetch(PDO::FETCH_ASSOC)) {
+
+//   echo $post['title'].PHP_EOL;
+// }
+
+$posts = $posts->fetchAll(PDO::FETCH_ASSOC);
+foreach ($posts as $post) {
+
+    echo $post['title'].PHP_EOL;
+}
